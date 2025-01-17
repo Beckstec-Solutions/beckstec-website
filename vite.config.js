d0353ac65c94f1dc0f25e9cfc22ee9a399bc7e3a
Benjamin <dotsehyde@gmail.com>
2025-01-17 20:00:08 +0000

    🐎 ci: Github Pages CI/CD

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  base: "beckstec-website",
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
   resolve: {
     alias: {
